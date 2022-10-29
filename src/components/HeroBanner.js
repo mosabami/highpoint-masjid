eee80f2c6721e9b53ed732a8efccddda2702fe35
Ayobami Ayodeji <ayobaami@hotmail.com>
2022-10-29 03:31:13 -0700

    fix warnings in CI

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,5 @@
 // import Link from "next/link"
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const navigation = [
  // { name: 'About us', href: '/about' },
