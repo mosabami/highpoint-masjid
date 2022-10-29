789a6300763e095569fd25826ced9eb64b0963c7
Ayobami Ayodeji <ayobaami@hotmail.com>
2022-10-29 14:02:40 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './components/Contact'
 import ContentSection from './components/ContentSection'
 import DonationSection from './components/DonationSection'
 import GoogleAnalytics from './components/GoogleAnalytics'
-import HeroBanner from './components/HeroBanner'
+import Navigation from './components/Navigation'
 import MosqueBrandingCSS from './components/MoqueBrandingCSS'
 import PrayerTimes from './components/PrayerTimes'
 import Team from './components/Team'
@@ -15,55 +15,30 @@ import mosques from './data/mosques.json'
 import React, { Component } from 'react';
 
 
-
-// function App({ data }) {
-//   return (
-//     <div className="App">
-//       <Helmet>
-//         <title>{data.name} - awesome Website</title>
-//         <GoogleAnalytics />
-//       </Helmet>
-
-//       <MosqueBrandingCSS data={data} />
-//       <HeroBanner data={data} />
-//       <PrayerTimes data={data} />
-//       <About data={data} />
-//       <ContentSection data={data} />
-//       <DonationSection data={data} />
-//       <Team data={data} />
-//       <Contact data={data} />
-//     </div>
-//   )
-// }
-
-// App.getInitialProps = async ({ req }) => {
-//   // const data =  mosques
-//   return { data: mosques }
-// }
-
 class App extends Component {
   constructor() {
     super();
     this.state = mosques
+    this.state.route = 'home'
   };
 
   render() {
-    let data = this.state
+    // let data = this.state
     return (
       <div className="App">
       <Helmet>
-        <title>{data.name} - awesome Website</title>
+        <title>{this.state.name} - Mosque Website</title>
         <GoogleAnalytics />
       </Helmet>
 
-      <MosqueBrandingCSS data={data} />
-      <HeroBanner data={data} />
-      <PrayerTimes data={data} />
-      <About data={data} />
-      <ContentSection data={data} />
-      <DonationSection data={data} />
-      <Team data={data} />
-      <Contact data={data} />
+      <MosqueBrandingCSS data={this.state} />
+      <Navigation data={this.state} />
+      <PrayerTimes data={this.state} />
+      <About data={this.state} />
+      <ContentSection data={this.state} />
+      <DonationSection data={this.state} />
+      <Team data={this.state} />
+      <Contact data={this.state} />
     </div>
     );
   }
