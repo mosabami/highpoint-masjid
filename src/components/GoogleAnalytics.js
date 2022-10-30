c53dd0db15e46e53efb05e913854aa5280d88c34
Ayobami Ayodeji <ayobaami@hotmail.com>
2022-10-30 00:26:17 -0700

    try

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,13 +1,15 @@
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_MEASUREMENT_ID ? process.env.REACT_APP_GOOGLE_MEASUREMENT_ID : 'unknown'
+
 export default function GoogleAnalytics() {
   return (<>
-    <script async src={`https://www.googletagmanager.com/gtag/js?id=G-MZ5SVK8ZJ7`}></script>
+    <script async src={`https://www.googletagmanager.com/gtag/js?id={TRACKING_ID}`}></script>
     <script
       dangerouslySetInnerHTML={{
       __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-GCS07570PB');
+        gtag('config', {TRACKING_ID});
       `,
       }}
     />
